ed3a52d6e517c41e7270efe8bae5b0da72b8cce6
Gabriel Guralnick <gabriel@abacus.ai>
2025-03-19 11:52:35 -0500

    replace with placeholder data

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/liveswebench-site/',
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.md'],
 })
